c025dfdb2c8930763426507de8cb016acb4da2a4
Eric Fournier <eric.fournier@fsaa.ulaval.ca>
2014-04-15 13:01:35 -0400

    Fix to transcriptomic circos plot generation.
    Add files for analysis of Paul, Julieta's results.
    Raise the number of "top features" represented by triangles from 100 to 200.
    Make transcriptomic p-value bedgraphs use a -log10 scale, like the epigenetic p-value bedgraphs.
    Fix issue with zip file generation with epigenetic-only experiments.

---
+++ RunExperiments/Julieta.sh
@@ -0,0 +1,2 @@
+./FullAnalysis.sh  "Julieta/Epigenetic" "Julieta.target" "Raw/Julieta/Epigenetic" \
+                   "Julieta/Transcriptomic" "Julieta.target" "Raw/Julieta/Transcriptomic" "Control" "Julieta/Combined" 2 0.01 1.5 0.05 v2
---
+++ RunExperiments/Paul.sh
@@ -0,0 +1,7 @@
+./FullAnalysis.sh "Paul/BMA/Epigenetic" "BMA.target" "Raw/Paul/Epigenetic" \
+                  "Paul/BMA/Transcriptomic" "BMA.target" "Raw/Paul/Transcriptomic" \
+                  "Control" "Paul/BMA/Combined" 1.5 0.05 v2         
+
+./FullAnalysis.sh "Paul/Carnitine/Epigenetic" "Carnitine.target" "Raw/Paul/Epigenetic" \
+                  "Paul/Carnitine/Transcriptomic" "Carnitine.target" "Raw/Paul/Transcriptomic" \
+                  "Control" "Paul/Carnitine/Combined" 1.5 0.05 v2         
