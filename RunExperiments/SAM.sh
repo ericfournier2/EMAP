3b3897f810afc8c78fbcb60ebbf98ed7c0b47b45
Eric Fournier <eric.fournier@fsaa.ulaval.ca>
2014-10-07 15:10:59 -0400

    Create new, more thorough SAM analysis parameters.

--- RunExperiments/SAM.sh
+++ RunExperiments/SAM.sh
@@ -1,2 +1,8 @@
-./Scripts/FullAnalysis.sh  "SAM/Epigenetic" "SAM.target" "Raw/SAM/Epigenetic" \
-                   "SAM/Transcriptomic" "SAM.target" "Raw/SAM/Transcriptomic" "Control" "SAM/Combined" 1.5 0.05 1.5 0.05 v1
+./Scripts/FullAnalysis.sh  "SAM/HpaII/Epigenetic" "HpaII.target" "Raw/SAM/Epigenetic" \
+                   "SAM/HpaII/Transcriptomic" "SAM.target" "Raw/SAM/Transcriptomic" "Control" "SAM/HpaII/Combined" 1.5 0.05 1.5 0.05 v1
+./Scripts/FullAnalysis.sh  "SAM/AciI/Epigenetic" "AciI.target" "Raw/SAM/Epigenetic" \
+                   "SAM/AciI/Transcriptomic" "SAM.target" "Raw/SAM/Transcriptomic" "Control" "SAM/AciI/Combined" 1.5 0.05 1.5 0.05 v1
+./Scripts/FullAnalysis.sh  "SAM/HinP1I/Epigenetic" "HinP1I.target" "Raw/SAM/Epigenetic" \
+                   "SAM/HinP1I/Transcriptomic" "SAM.target" "Raw/SAM/Transcriptomic" "Control" "SAM/HinP1I/Combined" 1.5 0.05 1.5 0.05 v1
+./Scripts/FullAnalysis.sh  "SAM/All/Epigenetic" "All.target" "Raw/SAM/Epigenetic" \
+                   "SAM/All/Transcriptomic" "SAM.target" "Raw/SAM/Transcriptomic" "Control" "SAM/All/Combined" 1.5 0.05 1.5 0.05 v1                   
